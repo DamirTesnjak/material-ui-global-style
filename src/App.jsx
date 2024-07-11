32f30d3894ae18cb83023c116d7ff68fc3fdf043
DamirTesnjak <damir.tesjak@gmail.com>
2024-07-11 13:16:19 +0200

    styled the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { Box, Grid, Link, ListItemButton, Typography } from "@mui/material";
+import { Box, Grid, Link, ListItem, ListItemButton, Typography } from "@mui/material";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import hljs from 'highlight.js';
 
 import { MuiClasses } from "./constants/muiClasses";
 import { components } from "./constants/components";
@@ -22,7 +23,7 @@ function App() {
         const muiComponentsClasses = Object.keys(MuiClasses);
         return (
             <Box sx={{
-                height: "88vh",
+                height: "80vh",
                 overflowY: "scroll",
             }}>
                 {
@@ -44,7 +45,7 @@ function App() {
         if (MuiClasses[muiClass]) {
             const classNames = Object.values(MuiClasses[muiClass]);
             return classNames.map((className) => {
-                return (<Typography key={className}>{className}</Typography>)
+                return (<ListItem key={className}>{className}</ListItem>)
             })
         }
     }
@@ -87,6 +88,22 @@ function App() {
         }
     }
 
+    function generateComponentLink() {
+        if (muiClass) {
+            const componentNameString = componentName(muiClass);
+            const componentNameKebabCase = componentNameString
+                .replace(/([a-z0–9])([A-Z])/g, "$1-$2").toLowerCase();
+            const link = `https://mui.com/material-ui/api/${componentNameKebabCase}/`;
+            return link;
+        }
+        return null;
+    }
+
+    const parseJSON = () => {
+        const hJson = `<pre>${hljs.highlight(`${JSON.stringify(themeStyleOverrideJSON(MuiClasses[muiClass]), undefined, 2)}`, { language: 'json' }).value}</pre>`;
+        return { __html: hJson || "" };
+    }
+
     return (
         <ThemeProvider
             theme={theme}
@@ -96,45 +113,65 @@ function App() {
                     MUI styles info explorer
                 </Typography>
                 <Grid container>
-                    <Grid container xs={2}>
+                    <Grid
+                        item
+                        xs={12}
+                        sm={12}
+                        md={2}
+                    >
+                        <p>
+                        This web app is a tool, to help you find class names for CSS styling and overview of
+                        keys of the JSON object for "ThemeProvider". For the latter, some
+                        keys in a JSON object of a selected component will simply not work. This is because, the gathered keys
+                        are collected from <code>"@mui/material"</code> library files and some of them are not included in component's API.
+                        </p>
+                        <p>
+                        To find out which keys are valid visit API page of selected component.
+                        The link is automatically generated for selected component,
+                        </p>
                         <Typography>
-                            This web app is a tool, to help you find class names for CSS styling and general overview of
-                            all valid keys when typing JSON for style override with a "ThemeProvider". For the latter, some
-                            keys in a JSON object of a selected component will simply not work. This is because, the gathered keys
-                            are collected from "@mui/material" library files and some of them are not included in components API.
-                            <br/>
-                            <br/>
-                            To find out which keys are valid visit the selected component API page:
-                            <br/>
-                            The link is automatically generated for a selected component
-
+                            {muiClass && (<Link href={generateComponentLink()} target="_blank">{`${componentName(muiClass)} API`}</Link>)}
                         </Typography>
                     </Grid>
-                    <Grid item xs={2} sx={{
-                                height: "88vh",
-                            }}>
-                        <Typography variant="h6">
+                    <Grid
+                        item
+                        xs={12}
+                        sm={12}
+                        md={2}
+                        sx={{
+                             height: "86vh",
+                        }}
+                    >
+                        <h5>
                             Components
-                        </Typography>
+                        </h5>
                         {displayComponentsClassNamesInfo()}
                     </Grid>
-                    <Grid container xs={8} sx={{
-                                height: "88vh",
-                            }}>
+                    <Grid 
+                        container
+                        xs={12}
+                        sm={12}
+                        md={8}
+                        sx={{
+                            height: "86vh",
+                        }}
+                    >
                         <Grid item xs={12}>
-                            <Typography variant="h5">
+                            <h5>
                                 {`Selected component ${componentName(muiClass)}`}
-                            </Typography>
+                            </h5>
                         </Grid>
                         <Grid
                             item
-                            xs={6}
+                            xs={12}
+                            sm={12}
+                            md={6}
                         >
-                            <Typography variant="h6">
+                            <h6>
                                 CSS Class Names
-                            </Typography>
+                            </h6>
                             <Box sx={{
-                                height: "88vh",
+                                height: "74vh",
                                 overflowY: "scroll",
                             }}>
                                 {themeStyleOverrideCSS()}
@@ -142,23 +179,24 @@ function App() {
                         </Grid>
                         <Grid
                             item
-                            xs={6}
+                            xs={12}
+                            sm={12}
+                            md={6}
                         >
-                            <Typography variant="h6">
+                            <h6>
                                 Style overrride
-                            </Typography>
+                            </h6>
                             <Typography>
                                 How to incoporate generated JSON, please visit: <br/>
-                                <Link href="https://mui.com/material-ui/customization/theme-components/#theme-default-props">Theme style overrides</Link>
+                                <Link href="https://mui.com/material-ui/customization/theme-components/#theme-default-props" target="_blank">Theme style overrides</Link>
                             </Typography>
                             <Box
                                 sx={{
-                                    height: "88vh",
+                                    height: "65vh",
                                     overflowY: "scroll",
-                                    whiteSpace: "pre"
                                 }}
                             >
-                                {JSON.stringify(themeStyleOverrideJSON(MuiClasses[muiClass]), null, "\t")}
+                             <div dangerouslySetInnerHTML={parseJSON()} />
                             </Box>
                         </Grid>
                     </Grid>
